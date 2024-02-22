67c7bf304696ee70a2808b4b5aae1e79aea8fd09
abdo160996 <abdo160996+1@gmail.com>
2024-02-22 13:11:12 +0200

    .

--- src/components/RatingStars.jsx
+++ src/components/RatingStars.jsx
@@ -2,30 +2,26 @@ import React from "react";
 import { TbStarFilled } from "react-icons/tb";
 import { TbStarHalfFilled } from "react-icons/tb";
 function RatingStars({ rating }) {
-    const stars = [];
-    const fullStars = Math.floor(rating)
-    const halfStars = rating - fullStars
-  
-    for (let i = 0; i < fullStars; i++) {
-        stars.push(<TbStarFilled className="text-yellow-500 text-lg"/>)
-    }
-    if (halfStars) {
-       
-        stars.push(<TbStarHalfFilled className="text-yellow-500 text-lg"/>)
-        
-    }
+  const stars = [];
+  const fullStars = Math.floor(rating);
+  const halfStars = rating - fullStars;
 
-    while(stars.length < 5) {
-        stars.push(<TbStarFilled className="text-neutral-300 text-lg"/>)
-    }
+  for (let i = 0; i < fullStars; i++) {
+    stars.push(<TbStarFilled className="text-yellow-500 text-lg" />);
+  }
+  if (halfStars) {
+    stars.push(<TbStarHalfFilled className="text-yellow-500 text-lg" />);
+  }
+
+  while (stars.length < 5) {
+    stars.push(<TbStarFilled className="text-neutral-300 text-lg" />);
+  }
   return (
-
-      <div className="rating ">
-        {stars.map((star, index) => (
-          <span key={index}>{star}</span>
-        ))}
-      </div>
-
+    <div className="rating ">
+      {stars.map((star, index) => (
+        <span key={index}>{star}</span>
+      ))}
+    </div>
   );
 }
 
