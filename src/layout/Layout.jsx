20b59a4ebee8c110f390694bbacef588ea8b647c
abdo160996 <abdo160996@gmail.com>
2025-01-15 13:18:56 +0200

    style: update HTML structure and enhance logo sizes in Login and Register forms

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -5,7 +5,7 @@ import Footer from "../components/Footer";
 import Navbar from '../components/Navbar'
 function Layout() {
   return (
-    <div className="flex flex-col min-h-screen bg-white">
+    <div className="flex flex-col min-h-screen bg-white ">
       <Navbar />
      <Outlet/>
       <Footer />
